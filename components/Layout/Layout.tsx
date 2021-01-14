57b5070b73d9444cfb3dda03c002f9239197de66
Andres Manco <amanco@vinformatix.com>
2021-01-14 13:26:34 -0600

    Added the root file(App) and CSS

---
+++ components/Layout/Layout.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '@components/Navbar/Navbar'
+
+const Layout: React.FC = ({ children }) => {
+  return (
+    <div>
+      <Navbar />
+      {children}
+      <footer className="container">This is the footer</footer>
+      <style jsx>{`
+        .container {
+          background: salmon;
+        }
+      `}</style>
+    </div>
+  )
+}
+export default Layout
